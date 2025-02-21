c8c7c77f8ea6db5fcd50e217b7db8e7d0f36aa6a
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-02-22 02:16:38 +0530

    Day 13 html,css interview prep

---
+++ HTML, CSS & JS Anurag Singh ProCodrr/Interview Lecture 3/script.js
@@ -0,0 +1,60 @@
+// * link of yt lecture :- https://youtu.be/sbDkLek2VjI?si=uzC9QK1kjkyPYHgn
+
+// TODO : Full Form of HTML
+// * Ans :- Hypertext Markup Language
+
+// TODO : Why we use html ?
+// * Ans :- To create structure of web page
+
+// TODO : What are Semantic tags in html ?
+// * Ans :- Tags that provide meaning to the content on the web page like header, footer etc
+
+// TODO : Why do we use Semantic tag ?
+// * Ans :- It improves the accessibility of the web page
+// * Ans :- It improves the search engine ranking
+// * Ans :- It improves the user experience
+
+// TODO : What are attributes in html ?
+// * Ans :- Attributes are used to provide additional information about an element
+
+// TODO : how do u create a hyperlinkin html ?
+// * Ans :- <a href="https://www.google.com">Google</a>
+
+// TODO : create a ordered list in html with fruits name ?
+// * Ans :- //  <ol type="a">
+/* <ol>
+  <li>Apple</li>
+  <li>Orange</li>
+  <li>Pineapple</li>
+</ol > */
+
+// TODO : Full form of css
+// * Ans :- Cascading Style Sheets
+
+// TODO : what does it mean by cascading ?
+// * Ans :- It means that the style of one element can be inherited by another element
+
+// TODO : difference b/w specificity & cascade ?
+// * Ans :- Specificity means that the style of one element will override the style of another element
+
+// TODO : explain box model ?
+// * Ans :- It is a model that is used to create a box around an element
+// * Ans :- we have margin, border, padding & content inside the box
+
+// TODO : difference b/w box-sizing:border-box & box-sizing:content-box ?
+// * Ans :- box-sizing: border-box" means that when you set the width and height of an element, it includes the padding and border within that specified size, while "box-sizing: content-box" (the default) means that the width and height only apply to the content itself, with padding and border added on top of that, making the element appear larger than the set dimensions
+
+// TODO : there is a div having width 200px & border 10px solid black what is the total width of the div in box-sizing:border-box & box-sizing:content-box ?
+// * Ans :- 220px in box-sizing:border-box & 200px in box-sizing:content-box
+// 1. box - sizing: content - box(Default)
+
+// Content width: 200px;
+// Border: 10px + 10px(left and right) = 20px
+// Total width: 200px + 20px = 220px
+// With content - box, the border and padding are added to the content width to calculate the total width of the element.
+
+// 2. box - sizing: border - box
+
+// Total width: 200px(This is the width including the border and padding.)
+
+// TODO : make a card with image using html & css ?
