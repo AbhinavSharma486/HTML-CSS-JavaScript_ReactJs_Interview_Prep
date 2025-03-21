861530775e6cd1580c2c132922f9d5b8168fc210
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-03-22 01:15:25 +0530

    day 17 of interview prep

---
+++ ReactJS Developer Interview Series/Interview No - 32/script.js
@@ -0,0 +1,192 @@
+// & Link of this interview youtube video :- https://youtu.be/oE-f0NLEVPE?si=RFuKtg3rbsMVS6A0
+
+// ^ Javascript :-
+
+// TODO :- what is map method ?
+
+// const numbers = [1, 2, 3, 4];
+
+// const abc1 = numbers.map(item => item = 2);
+// console.log(abc1); // [2, 2, 2, 2]
+
+// const abc2 = numbers.filter(item => item = 2);
+// console.log(abc2); // [1, 2, 3, 4]
+
+// const abc3 = numbers.reduce(item => item = 5);
+// console.log(abc3); // 2
+
+// TODO :- Difference between async , await & promise ?
+// * Ans :- async is a keyword that is used to create asynchronous functions. await is a keyword that is used to wait for a promise to resolve.
+
+// TODO :- what is the basic way to crate json file ?
+// * Ans :- by giving tha file name and writing the json data in it.
+
+// TODO :- can javascript objects can be converted into json ?
+// * Ans :- yes, you can convert JavaScript objects into JSON by using the JSON.stringify() method.
+
+// TODO :- what is lexical scope ?
+// * Ans :- The lexical scope of a variable is the part of the program where the variable is defined.
+
+// TODO :- when we receive refrence error ?
+// * Ans :- ReferenceError: variable is not defined
+
+// TODO :- features of es6 ?
+// * Ans :- promises, setTimeout , setInterval , splice , slice , map , reduce , filter
+
+// TODO :- what is temporal dead zone ?
+// * Ans :- The temporal dead zone is a period of time in which a variable declared using let or const cannot be accessed before initialization.
+
+// TODO :-
+// var a = 10;
+// console.log(a++); // 10
+// console.log(a); // 11
+
+// TODO :- what is the type of NAN ?
+// * Ans :- NaN is a special numeric value that indicates that a number is not a valid number. In JavaScript, NaN is a number that is not a legal number.
+
+// TODO :- difference between local storage & session storage ?
+// * Ans :- The localStorage object is used to store data on the client-side, while the sessionStorage object is used to store data on the client-side for a single session.
+
+// TODO :- Return the longest string from the given string ?
+// let str = "This is Abhinav Sharma and i am study in Gaytri Devi College of Engineering";
+// let arr = str.split(" ");
+// let longest = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i].length > longest.length) {
+//     longest = arr[i];
+//   }
+// }
+
+// console.log(longest);
+
+// TODO :- what is every method with example ?
+// * Ans :- The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value.
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let result = arr.every(function (element) {
+//   return element > 1;
+// });
+
+// console.log(result); // false
+
+// TODO :- what is some method with example ?
+// * Ans :- The some() method tests whether some elements in the array pass the test implemented by the provided function. It returns a Boolean value.
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let result = arr.some(function (element) {
+//   return element > 4;
+// });
+
+// console.log(result); // true
+
+// TODO :- u are having an array with elements 1 2 3 4 you want to add 5 in the end of the array how u do it ?
+// let arr = [1, 2, 3, 4];
+// arr.unshift(5); // unshift method is used to add element at the beginning of the array
+// arr.push(5); // push method is used to add element at the end of the array
+// arr.push(3);
+// arr.shift(); // shift method is used to remove element from the beginning of the array
+// console.log(arr);
+
+// -------------------------------------------------------------------------------
+
+// ^ ReactJs :-
+
+// TODO :- hwo will u rate your self in reactjs ?
+// * Ans :- 7/10;
+
+// TODO :- for api integration what u use ?
+// * Ans :- axios
+
+// TODO :- which is better & why axios or fetch ?
+// * Ans :- axios is better because it is a promise based library. It is easier to use than fetch. It is also faster than fetch. It is also more secure than fetch. It is also more secure than fetch.
+
+// TODO :- what is the difference between react routing & conventional routing ?
+// * Ans :- The main difference between React routing and conventional routing is that React routing is a single page application (SPA) routing system, while conventional routing is a multi-page routing system.
+
+// TODO :- do we need to import for routes ?
+// * Ans :- yes we need to import for routes in reactjs.
+
+// TODO :- what do u mean by prop drealling ?
+// * Ans :- prop drilling is a technique used in React to pass data from parent component to child component.
+
+// TODO :- what is jsx ?
+// * Ans :- JSX is a syntax extension for JavaScript that allows you to write HTML-like code in your JavaScript files. It is a way to write HTML in JavaScript, but it is not HTML.
+
+// TODO :- what is the use of bable ?
+// * Ans :- Babel is a JavaScript transpiler that converts modern JavaScript code into older JavaScript code that is compatible with older browsers. It is used to write modern JavaScript code in a way that is compatible with older browsers.
+
+// TODO :- in useState we write setState what is setState ?
+// * Ans :- setState is a function that is used to update the state of a component. It is called when the state of a component needs to be updated.
+
+// TODO :- Difference between real dom & virtual dom ?
+// * Ans :- The main difference between real DOM and virtual DOM is that the real DOM is the actual DOM of the page, while the virtual DOM is a copy of the DOM that is used to render the page.
+
+// TODO :- compitetor of reactjs ?
+// * Ans :- Next.js, Vue.js, Svelte.js and Angular.js
+
+// TODO :- difference between reactjs & angularjs ?
+// * Ans :- The main difference between React and Angular is that React is a JavaScript library for building user interfaces, while Angular is a framework for building single-page applications. Both are used for building web applications.
+
+// TODO :- what is the difference between framework & library ?
+// * Ans :- The main difference between framework and library is that framework is a collection of libraries, while library is a single library.
+
+// -----------------------------------------------------------------------------
+
+// ^ HTML :-
+
+// TODO :- html layout structure ?
+// * Ans :- header, main , doctype , html , header , body , metatag , title , div container , nav container , footer
+
+// TODO :- how can combine 2 or more rows & 2 or more columns in html ?
+// * Ans :- using the special attributes colspan and rowspan
+
+// TODO :- what do u mean by row span & column span ?
+// * Ans :- row span is a way to combine 2 or more rows in html. column span is a way to combine 2 or more columns in html.
+
+// TODO :- various formatting text in html ?
+// * Ans :- b , i , u , em , strong , small , big , sub , sup , del , ins , mark
+
+// TODO :- difference between strong tag & bold tag ?
+// * Ans :- The main difference between strong tag and bold tag is that strong tag is used for important text, while bold tag is used for bold text.
+
+// TODO :- difference between em tag & i tag ?
+// * Ans :- The main difference between em tag and i tag is that em tag is used for emphasis, while i tag is used for italics.
+
+// TODO :- difference between figure tag & image tag ?
+// * Ans :- The <figure> tag in HTML is a semantic container for images, illustrations, diagrams, or code snippets, often used with a caption (<figcaption>), while the <img> tag simply embeds an image into a document.
+
+// TODO :- what are empty elements ?
+// * Ans :- Empty elements are elements that do not have any content, such as <br>, <hr>, <img>, <input>, <link>, <meta>, <source>, <track>, and <wbr>.
+
+// ------------------------------------------------------------------
+
+// ^ CSS :-
+
+// TODO :- why we use keyframes ?
+// * Ans :- keyframes is a way to create animations in CSS. It is a way to create animations in CSS.
+
+// TODO :- how can we add gradients in css ?
+// * Ans :- by using the linear-gradient function in css.
+
+// TODO :- how many ways to add gradients in css ?
+// * Ans :- Linear Gradients (goes down/up/left/right/diagonally)
+// * Ans :- Radial Gradients (defined by their center)
+// * Ans :- Conic Gradients (rotated around a center point)
+
+// TODO :- what is universal selector ?
+// * Ans :- The universal selector * is a CSS selector that matches all elements on a page. It is used to select all elements on a page.
+
+// TODO :- what is specificity in css ?
+// * Ans :- The specificity of a CSS selector is a way to determine the priority of a selector in a CSS document. It is a way to determine the priority of a selector in a CSS document.
+
+// TODO :- what are attributes ?
+// * Ans :- The attributes of a CSS selector are a way to add additional information to a selector. It is a way to add additional information to a selector.
+
+// TODO :- priority order in css ?
+// * Ans :-  inline styles (highest) > ID selectors > class selectors, pseudo-classes, and attributes > tag selectors (lowest)
+
+// TODO :- how can we overlap elements in css ?
+// * Ans :- by using the z-index property.
